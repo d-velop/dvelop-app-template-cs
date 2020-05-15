d005f47c6ac74269f96eddea2ae05f410049196c
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2020-05-15 21:36:06 +0200

    WIP

--- Remote/Controller/AppSession/AppSessionController.cs
+++ Remote/Controller/AppSession/AppSessionController.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Dvelop.Domain.Repositories;
+using Dvelop.Sdk.IdentityProvider.Client;
+using Dvelop.Sdk.IdentityProvider.Dto;
 using Dvelop.Sdk.SigningAlgorithms;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace Dvelop.Remote.Controller.AppSession
 {
@@ -22,14 +23,22 @@ namespace Dvelop.Remote.Controller.AppSession
         private readonly IUserRepository _user;
 
         private readonly ILogger<AppSessionController> _log;
-        private readonly HttpClient _client;
+        private readonly IdentityProviderClient _idpClient;
 
+        private static readonly Dictionary<string, string> AppSessionStore = new Dictionary<string, string>();
+        
+        
         public AppSessionController(IConfiguration configuration, ITenantRepository tenantRepository, IUserRepository user, IHttpClientFactory client, ILoggerFactory factory)
         {
             _configuration = configuration;
             _tenantRepository = tenantRepository;
+
             _user = user;
-            _client = client.CreateClient("test");
+            _idpClient = new IdentityProviderClient( client, () => new TenantInformation
+            {
+                TenantId = tenantRepository.TenantId,
+                SystemBaseUri = tenantRepository.SystemBaseUri.ToString()
+            } );
             _log = factory.CreateLogger<AppSessionController>();
         }
         
@@ -37,6 +46,13 @@ namespace Dvelop.Remote.Controller.AppSession
         [HttpGet("appsession", Name = nameof(AppSessionController) + "." + nameof(CreateAppSession))]
         public async Task<IActionResult> CreateAppSession()
         {
+            _log.LogInformation($"Requesting AppSession");
+            
+            // Lookup a persistent Database, which should be tenant and expire aware.
+            if (AppSessionStore.ContainsKey(_tenantRepository.TenantId) && !string.IsNullOrWhiteSpace(AppSessionStore[_tenantRepository.TenantId]))
+            {
+                return Ok(AppSessionStore[_tenantRepository.TenantId]);
+            }
             var requestId = Guid.NewGuid().ToString("N");
 
             var route = Url.RouteUrl(nameof(AppSessionController) + "." + nameof(Callback), new {requestId});
@@ -47,28 +63,24 @@ namespace Dvelop.Remote.Controller.AppSession
                 AppName = _configuration["APP_NAME"],
                 Callback = new Uri( route , UriKind.Relative)
             };
+            _log.LogInformation("RequestId: " + appSessionRequestDto.RequestId);
+            _log.LogInformation("AppName: " + appSessionRequestDto.AppName);
+            _log.LogInformation("CallBack: " + appSessionRequestDto.Callback);
             
-            var request = new HttpRequestMessage(HttpMethod.Post, new Uri(_tenantRepository.SystemBaseUri, "/identityprovider/appsession"))
+            if (await _idpClient.RequestAppSession( appSessionRequestDto ))
             {
-                Content = new StringContent(
-                    JsonConvert.SerializeObject(appSessionRequestDto,
-                        new JsonSerializerSettings
-                        {
-                            ContractResolver = new DefaultContractResolver
-                            {
-                                NamingStrategy = new LowercaseNamingStrategy()
-                            }
-                        }), Encoding.UTF8, "application/json"),
-            };
-            request.Headers.Add("Origin", _tenantRepository.SystemBaseUri.ToString().TrimEnd('/'));
-
-            var response = await _client.SendAsync(request);
-              
-            if (response.IsSuccessStatusCode)
-            {
-                return Ok();
+                _log.LogInformation("successfully created an AppSession for " + _tenantRepository.TenantId);
+                _log.LogInformation($"number of AppSessions in Store: {AppSessionStore.Count}");
+                foreach (var keyValuePair in AppSessionStore)
+                {
+                    _log.LogInformation( $"{keyValuePair.Key} -> {keyValuePair.Value} " );
+                }
+                var appSessionNotYetAvailable = (AppSessionStore.ContainsKey(_tenantRepository.TenantId) && !string.IsNullOrWhiteSpace(AppSessionStore[_tenantRepository.TenantId]) 
+                    ? AppSessionStore[_tenantRepository.TenantId] : "AppSession not yet available");
+                return Ok(appSessionNotYetAvailable);
             }
-            return StatusCode((int) response.StatusCode, response.ReasonPhrase);
+            _log.LogWarning("There was an error while creating an AppSession");
+            return BadRequest("The AppSession could not be created");
         }
         
         [AllowAnonymous]
@@ -80,42 +92,21 @@ namespace Dvelop.Remote.Controller.AppSession
             _log.LogInformation( "Expire: "+callback.Expire);
             _log.LogInformation( "RequestId: " + requestId );
             var hashedRequestId =  HmacSha256Algorithm.HmacSha256(Convert.FromBase64String(_configuration["SIGNATURE_SECRET"]), requestId);
-            var mySign = HmacSha256Algorithm.Sha256(
-                $"{_configuration["APP_NAME"]}{callback.AuthSessionId}{callback.Expire}{hashedRequestId}");
+            var mySign = HmacSha256Algorithm.Sha256($"{_configuration["APP_NAME"]}{callback.AuthSessionId}{callback.Expire}{hashedRequestId}");
             
-            // Hex(Sha256(<appname><authSessionId><expire><requestid>
-            
-            _log.LogInformation($"sign valid? {mySign==callback.Sign}");
-            if (mySign == callback.Sign)
+            _log.LogInformation($"signature valid? {mySign==callback.Sign}");
+            if (mySign != callback.Sign)
             {
-                return Ok(new StringContent(callback.AuthSessionId));
-            }
-            else
-            {
+                _log.LogWarning("Signature mismatch");
                 return BadRequest("Sign mismatch");
             }
+            _log.LogInformation($"number of AppSessions in Store: {AppSessionStore.Count}");
+            foreach (var keyValuePair in AppSessionStore)
+            {
+                _log.LogInformation( $"{keyValuePair.Key} -> {keyValuePair.Value} " );
+            }
+            AppSessionStore[_tenantRepository.TenantId] = callback.AuthSessionId;
+            return Ok(new StringContent("Got an AppSession", Encoding.UTF8, "application/json"));
         }
-    }
-
-    public class LowercaseNamingStrategy : NamingStrategy
-    {
-        protected override string ResolvePropertyName(string name)
-        {
-            return name.ToLowerInvariant();
-        }
-    }
-
-    public class AppSessionRequestDto
-    {
-        public string AppName { get; set; }
-        public Uri Callback { get; set; }
-        public string RequestId { get; set; }
-    }
-    
-    public class AppSessionCallbackDto
-    {
-        public string AuthSessionId { get; set; }
-        public string Expire { get; set; }
-        public string Sign { get; set; }
     }
 }
--- Remote/Startup.cs
+++ Remote/Startup.cs
@@ -8,6 +8,7 @@ using System.Text.Json;
 using Dvelop.Sdk.TenantMiddleware;
 using Dvelop.Domain.Repositories;
 using Dvelop.Remote.Constraints;
+using Dvelop.Remote.Controller.AppSession;
 using Dvelop.Remote.Filter;
 using Dvelop.Sdk.IdentityProvider.Client;
 using Dvelop.Sdk.IdentityProvider.Middleware;
@@ -36,47 +37,52 @@ namespace Dvelop.Remote
     public class Startup
     {
         private IConfiguration Configuration { get; }
-        
+
         private readonly ICustomServiceProviderFactory _factory;
 
         private readonly ILogger<Startup> _logger;
 
-        public Startup(IConfiguration configuration, ICustomServiceProviderFactory factory, ILoggerFactory loggerFactory)
+        public Startup(IConfiguration configuration, ICustomServiceProviderFactory factory,
+            ILoggerFactory loggerFactory)
         {
             _factory = factory;
-            
+
             _logger = loggerFactory.CreateLogger<Startup>();
-            
+
             Configuration = configuration;
 
-            Configuration["DEFAULT_SYSTEM_BASE_URI"] = Configuration["SYSTEMBASEURI"]??"http://localhost";
-            Configuration["SIGNATURE_SECRET"] = Configuration["SIGNATURE_SECRET"]??"";
+            Configuration["DEFAULT_SYSTEM_BASE_URI"] = Configuration["SYSTEMBASEURI"] ?? "http://localhost";
+            Configuration["SIGNATURE_SECRET"] = Configuration["SIGNATURE_SECRET"] ?? "";
 
-            Configuration["APP_NAME"] = Configuration["APP_NAME"]??"acme-apptemplatecs";
+            Configuration["APP_NAME"] = Configuration["APP_NAME"] ?? "acme-apptemplatecs";
             Configuration["BASE"] = $"/{Configuration["APP_NAME"]}";
-            Configuration["ASSETS"] = Configuration["ASSET_BASE_PATH"]??$"{Configuration["DEFAULT_SYSTEM_BASE_URI"]}{Configuration["BASE"]}";
-            
+            Configuration["ASSETS"] = Configuration["ASSET_BASE_PATH"] ??
+                                      $"{Configuration["DEFAULT_SYSTEM_BASE_URI"]}{Configuration["BASE"]}";
+
             _logger.LogInformation($"SYSTEMBASEURI: {Configuration["SYSTEMBASEURI"]}");
             _logger.LogInformation($"DEFAULT_SYSTEM_BASE_URI: {Configuration["DEFAULT_SYSTEM_BASE_URI"]}");
-            _logger.LogInformation($"SIGNATURE_SECRET set: {!string.IsNullOrWhiteSpace(Configuration["SIGNATURE_SECRET"])}");
+            _logger.LogInformation(
+                $"SIGNATURE_SECRET set: {!string.IsNullOrWhiteSpace(Configuration["SIGNATURE_SECRET"])}");
             _logger.LogInformation($"APP_NAME: {Configuration["APP_NAME"]}");
             _logger.LogInformation($"ASSETS: {Configuration["ASSETS"]}");
             _logger.LogInformation($"BASE: {Configuration["BASE"]}");
         }
 
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            
+
             // Add Filter for DvSignature
             services.AddScoped<Dv1HmacSha256SignatureFilter>();
-            
+
             // Add HttpClientService
             services.AddHttpClient();
-            services.AddHttpClient("test");
             
+            // Add a known HttpClient for IdentityProvider 
+            services.AddHttpClient(IdentityProviderClient.APPNAME);
+
             // Allow Classes to access the HttpContext
             services.AddHttpContextAccessor();
             services.TryAddEnumerable(ServiceDescriptor.Singleton<MatcherPolicy, ProducesMatcherPolicy>());
@@ -87,7 +93,7 @@ namespace Dvelop.Remote
                 options.DefaultChallengeScheme = "IdentityProvider";
                 options.DefaultForbidScheme = "IdentityProvider";
 
-            }).AddIdentityProviderAuthentication("IdentityProvider", "d.velop Identity Provider", options => {  });
+            }).AddIdentityProviderAuthentication("IdentityProvider", "d.velop Identity Provider", options => { });
             services.AddAuthorization(options =>
             {
                 // DefaultPolicy will be evaluated, if there is an [Authorize] attribute, but no configuration.
@@ -96,17 +102,14 @@ namespace Dvelop.Remote
                     .Build();
 
                 // FallbackPolicy will be evaluated, if there is neither [Authorize] nor an [AllowAnonymous] attribute provided.
-                options.FallbackPolicy= new AuthorizationPolicyBuilder()
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .Build();
             });
             // Create and configure Mvc/Razor
             services.AddRazorPages()
                 .AddMvcOptions(options => options.RespectBrowserAcceptHeader = true)
-                .AddRazorPagesOptions(options =>
-                {
-                    options.Conventions.AllowAnonymousToPage("/Error");
-                })
+                .AddRazorPagesOptions(options => { options.Conventions.AllowAnonymousToPage("/Error"); })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddJsonOptions(options =>
                 {
@@ -114,26 +117,28 @@ namespace Dvelop.Remote
                     options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
                     options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
                 });
-                /* If you want to switch back to NewtonSoftJson, use to following settings
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-                })*/
-                
+            /* If you want to switch back to NewtonSoftJson, use to following settings
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            })*/
+
             services.AddDirectoryBrowser();
             services.AddLogging(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Information));
-            services.AddRouting(routeOptions => routeOptions.AppendTrailingSlash = true );
+            services.AddRouting(routeOptions => routeOptions.AppendTrailingSlash = true);
             return _factory.CreateServiceProvider(services);
         }
 
         // This method gets called by the ASP .NET core runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IActionDescriptorCollectionProvider  actionDescriptorProvider)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            IActionDescriptorCollectionProvider actionDescriptorProvider)
         {
             // Print information about bound routes and the Controller, they are bound to.
-            var routes = actionDescriptorProvider.ActionDescriptors.Items.Where(ad => ad.AttributeRouteInfo != null).ToList();
+            var routes = actionDescriptorProvider.ActionDescriptors.Items.Where(ad => ad.AttributeRouteInfo != null)
+                .ToList();
             routes.ForEach(ad =>
             {
                 _logger.LogInformation($"{ad.AttributeRouteInfo.Template} -> '/{ad.AttributeRouteInfo.Name}'");
@@ -143,32 +148,33 @@ namespace Dvelop.Remote
             // If running without 'api_custom_domains'-Feature, the requests need to be rewritten to omit the
             // API-Gateway stage /prod or /dev, if running as a d.velop cloud App within a tenant aware environment.
             app.UseRewriter(new RewriteOptions()
-                
-                // Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction sets the BasePath-property to the name of the API-Gateway Stage.
-                // This RewriteRule can or should be removed, if: 
-                //   - The 'api_custom_domains'-Feature is activated
-                //   - There is no url-Rewriting Reverse-Proxy (You call the Api-Gateway Url directly).
-                .Add(rc =>
+
+                    // Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction sets the BasePath-property to the name of the API-Gateway Stage.
+                    // This RewriteRule can or should be removed, if: 
+                    //   - The 'api_custom_domains'-Feature is activated
+                    //   - There is no url-Rewriting Reverse-Proxy (You call the Api-Gateway Url directly).
+                    .Add(rc =>
                     {
                         var oldPathBase = rc.HttpContext.Request.PathBase;
-                        
+
                         // Set the RawTarget, because in an AWS Lambda it is not available (yet)
                         // See: https://github.com/aws/aws-lambda-dotnet/issues/656
                         var requestFeature = rc.HttpContext.Features.Get<IHttpRequestFeature>();
                         requestFeature.RawTarget ??= rc.HttpContext.Request.GetEncodedPathAndQuery();
-                        
+
                         rc.HttpContext.Request.PathBase = "";
-                        _logger.LogInformation($"Changed PathBase from '{oldPathBase}' to '{rc.HttpContext.Request.PathBase}'");
+                        _logger.LogInformation(
+                            $"Changed PathBase from '{oldPathBase}' to '{rc.HttpContext.Request.PathBase}'");
                         rc.Result = RuleResult.ContinueRules;
                     })
 
                 // This redirect ensures, that a URL is always used with an trailing '/', expect in the last segment ist a '.'.
                 // .AddRedirect(@"^(((.*/)|(/?))[^/.]+(?!/$))$", "$1/",302)
             );
-            
+
             // This will a a virtual path-segment to the application
             app.UsePathBase(Configuration["BASE"]);
-            
+
             // Enable Multi-Tenancy
             app.UseTenantMiddleware(new TenantMiddlewareOptions
             {
@@ -184,11 +190,13 @@ namespace Dvelop.Remote
                     tenantRepository.TenantId = tenantId;
                 }
             });
+            
             // Enable d.ecs IdentityProvider
+            var httpClientFactory = app.ApplicationServices.GetService<IHttpClientFactory>();
             app.UseIdentityProvider(new IdentityProviderOptions
             {
                 BaseAddress = new Uri(Configuration["DEFAULT_SYSTEM_BASE_URI"]),
-                HttpClient = new HttpClient(),
+                HttpClient = httpClientFactory.CreateClient(IdentityProviderClient.APPNAME),
                 TenantInformationCallback = () =>
                 {
                     var tenantRepository = app.ApplicationServices.GetService<ITenantRepository>();
@@ -198,7 +206,7 @@ namespace Dvelop.Remote
                         TenantId = tenantRepository.TenantId,
                         SystemBaseUri = tenantRepository.SystemBaseUri.ToString()
                     };
-                    
+
                 }
             });
 
@@ -221,13 +229,13 @@ namespace Dvelop.Remote
                 // cf. https://tools.ietf.org/html/rfc7234#section-4.1
                 // accept is added because most resources deliver JSON and HTML from the same URI
                 // x-dv-sig-1 ist added because most of the responses are tenant specific
-                httpContext.Response.Headers.Append("vary", new[] { "accept", "accept-language", "x-dv-sig-1"});
-                _logger.LogDebug($"{httpContext.Request.Method} ->  {httpContext.Request.Path}" );
+                httpContext.Response.Headers.Append("vary", new[] {"accept", "accept-language", "x-dv-sig-1"});
+                _logger.LogDebug($"{httpContext.Request.Method} ->  {httpContext.Request.Path}");
                 await next.Invoke();
             });
-            
+
             app.UseCookiePolicy();
-            
+
             app.UseRequestLocalization(new RequestLocalizationOptions
             {
                 DefaultRequestCulture = new RequestCulture("en-US"),
@@ -258,7 +266,9 @@ namespace Dvelop.Remote
             {
                 return;
             }
-            if (string.IsNullOrWhiteSpace(Configuration["RELATIVE_ASSET_DIR"])) // If no relative Asset Dir configured, use default.
+
+            if (string.IsNullOrWhiteSpace(Configuration["RELATIVE_ASSET_DIR"])
+            ) // If no relative Asset Dir configured, use default.
             {
                 app.UseStaticFiles();
             }
@@ -270,15 +280,15 @@ namespace Dvelop.Remote
 
                 app.UseStaticFiles(new StaticFileOptions
                 {
-                    FileProvider = new PhysicalFileProvider(wwwroot), 
+                    FileProvider = new PhysicalFileProvider(wwwroot),
                     RequestPath = ""
                 });
-                
+
                 if (env.IsDevelopment())
                 {
                     app.UseDirectoryBrowser(new DirectoryBrowserOptions
                     {
-                        FileProvider = new PhysicalFileProvider(wwwroot), 
+                        FileProvider = new PhysicalFileProvider(wwwroot),
                         RequestPath = ""
                     });
                 }
