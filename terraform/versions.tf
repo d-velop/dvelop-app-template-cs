0703466fb2ae14993821474e54f152d1e701dc36
Kaiser Daniel <Daniel.Kaiser@d-velop.de>
2021-08-05 13:31:29 +0200

    added default, local directory structure for terraform providers for local provider loading

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,19 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.52.0"
-    }
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = "~> 0.9.1"
+      version = "3.52.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2.0"
+      version = "2.2.0"
     }
     template = {
       source  = "hashicorp/template"
-      version = "~> 2.2.0"
+      version = "2.2.0"
     }
   }
 }
